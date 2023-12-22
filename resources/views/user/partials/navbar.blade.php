4ddf935b09afea289a5b47a0ed240775dfd878bc
Ferry Febriansyah <perriebenhard@gmail.com>
2023-12-22 08:46:43 +0700

    fixing Navbar

--- resources/views/user/partials/navbar.blade.php
+++ resources/views/user/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <header class="sticky top-0 z-50">
-    <nav class="flex justify-between px-20 py-4 backdrop-blur-md">
+    <nav id="navbar" class="flex justify-between px-20 py-4 backdrop-blur-md transition-all">
         <a href="{{ route('dashboard') }}">
             <div class="flex items-center justify-center gap-4">
                 <img src="{{ url('/img/logo.webp') }}" alt="Hello Kids" class="w-20">
@@ -20,7 +20,8 @@
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
 
-                <button class="px-8 py-2 font-semibold text-white transition-all border-2 rounded-lg hover:text-primary bg-primary hover:bg-slate-100 border-primary"
+                <button
+                    class="px-8 py-2 font-semibold text-white transition-all border-2 rounded-lg hover:text-primary bg-primary hover:bg-slate-100 border-primary"
                     href="route('logout')" onclick="event.preventDefault();this.closest('form').submit();">
                     Logout
                 </button>
@@ -28,3 +29,17 @@
         </div>
     </nav>
 </header>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var navbar = document.getElementById("navbar");
+
+        window.addEventListener("scroll", function() {
+            if (window.scrollY > 0) {
+                navbar.classList.add("shadow-lg");
+            } else {
+                navbar.classList.remove("shadow-lg");
+            }
+        });
+    });
+</script>
